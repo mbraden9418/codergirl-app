2afb18dc0ef5a802baa40b4c049fe26e2cfed759
user <mbraden9418@yahoo.com>
2018-10-03 19:15:54 -0500

    loaded employees from http reuests

--- src/app/dashboard/video-list/video-list.component.ts
+++ src/app/dashboard/video-list/video-list.component.ts
@@ -1,204 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-const videos = [
-  {
-    'title': 'Angular Observable Data Flow',
-    'author': 'Kyle Cordes',
-    'id': 'JPuqluYYa-o',
-    'date': '11/0Div3/2015',
-    'viewDetails': [
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 27,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 47,
-        'region': 'Europe',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-25'
-      }
-    ]
-  },
-  {
-    'title': 'Angular Performance Checklist',
-    'author': 'Paul Spears',
-    'id': 'cxqRijt9LbQ',
-    'date': '08/25/2017',
-    'viewDetails': [
-      {
-        'age': 36,
-        'region': 'North America',
-        'date': '2016-06-23'
-      },
-      {
-        'age': 30,
-        'region': 'North America',
-        'date': '2016-06-23'
-      },
-      {
-        'age': 54,
-        'region': 'North America',
-        'date': '2016-07-23'
-      },
-      {
-        'age': 43,
-        'region': 'Europe',
-        'date': '2016-0-24'
-      },
-      {
-        'age': 32,
-        'region': 'North America',
-        'date': '2016-08-24'
-      },
-      {
-        'age': 32,
-        'region': 'North America',
-        'date': '2016-08-25'
-      }
-    ]
-  },
-  {
-    'title': 'Live App Updates Without The App Store',
-    'author': 'Sani Yusuf',
-    'id': 's10wrXA-a7Y',
-    'date': '02/05/2018',
-    'viewDetails': [
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 27,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 47,
-        'region': 'Europe',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-25'
-      }
-    ]
-  },
-  {
-    'title': 'Angular Reactive Forms',
-    'author': 'Jack Balbes',
-    'id': 'A_Rq6ZsoXpI',
-    'date': '02/21/2017',
-    'viewDetails': [
-      {
-        'age': 36,
-        'region': 'North America',
-        'date': '2016-06-23'
-      },
-      {
-        'age': 30,
-        'region': 'North America',
-        'date': '2016-06-23'
-      },
-      {
-        'age': 54,
-        'region': 'North America',
-        'date': '2016-07-23'
-      },
-      {
-        'age': 43,
-        'region': 'Europe',
-        'date': '2016-0-24'
-      },
-      {
-        'age': 32,
-        'region': 'North America',
-        'date': '2016-08-24'
-      },
-      {
-        'age': 32,
-        'region': 'North America',
-        'date': '2016-08-25'
-      }
-    ]
-  },
-  {
-    'title': 'Imperative to Reactive with Angular and RxJS',
-    'author': 'John Baur',
-    'id': 'VJOPsjlbhdg',
-    'date': '05/28/2017',
-    'viewDetails': [
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 27,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-23'
-      },
-      {
-        'age': 47,
-        'region': 'Europe',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 37,
-        'region': 'North America',
-        'date': '2016-03-24'
-      },
-      {
-        'age': 17,
-        'region': 'North America',
-        'date': '2016-03-25'
-      }
-    ]
-  }
-] ;
+const API_URL = 'https://api.angularbootcamp.com';
 
+interface Video {
+  title: string;
+  author: string;
+  id: string;
+  viewDetails: ViewDetail[];
+}
+
+interface ViewDetail {
+  age: number;
+  region: string;
+  date: string;
+}
 @Component({
   selector: 'cg-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent {
-  videoList = videos;
+  selectedVideoId: string | undefined;
+  videoList = [];
 
+  constructor(http: HttpClient) {
+    http
+    .get<Video[]>(API_URL + '/videos')
+    .subscribe(videos => this.videoList = videos);
+   }
 }
