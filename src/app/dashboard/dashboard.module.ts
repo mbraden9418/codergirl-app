2386beb45e5305002793b09a77b4c672817c17ee
user <mbraden9418@yahoo.com>
2018-12-03 17:42:01 -0600

    added user forms

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,7 +6,7 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
 import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 import { RouterModule, Routes} from '@angular/router';
 import { VideoListItemComponent } from './video-list-item/video-list-item.component';
-
+import {ReactiveFormsModule} from '@angular/forms';
 const routes: Routes = [
   { path: '', component: VideoDashboardComponent, pathMatch: 'full' }
 ];
@@ -14,7 +14,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent, VideoListItemComponent]
 })
--- src/app/dashboard/stat-filters/stat-filters.component.ts
+++ src/app/dashboard/stat-filters/stat-filters.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'cg-stat-filters',
   templateUrl: './stat-filters.component.html',
   styleUrls: ['./stat-filters.component.css']
 })
+
 export class StatFiltersComponent implements OnInit {
+  details: FormGroup;
+  videoRatings = [
+    'Love',
+    'Like',
+    'Meh',
+    'Needs Improvement'
+  ];
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.details = fb.group({
+      firstName: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      jobTitle: ['', Validators.minLength(3)],
+      videoRatings: [''],
+    });
+  }
 
+  saveUserDetails(): void {
+    console.log('Form Submitted', this.details.value);
+  }
+
+  logForm(): void {
+    console.log('form:', this.details);
+  }
   ngOnInit() {
   }
 
